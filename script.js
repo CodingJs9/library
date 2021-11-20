c3f6bd71b40fdff7516ab76a5ddd242951a56b2b
Rithvik <rithvikr98@gmail.com>
2021-11-20 12:21:32 +0530

    Added render function to render contents of myLibrary array

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 const addBookBtn = document.querySelector('.add-book')
-const booksGrid = document.querySelector('.books')
+const booksGrid = document.querySelector('.books-grid')
 
 let myLibrary = []
 
 class Book {
-	constructor(name, author, pages) {
-		this.name = name
+	constructor(title, author, pages) {
+		this.title = title
 		this.author = author
-		this.pages = pages
+		this.numOfPages = pages
 	}
 }
 
-function addBookToLibrary(name, author, pages) {
-	let book = new Book(name, author, pages)
+function addBookToLibrary(title, author, pages) {
+	let book = new Book(title, author, pages)
 	myLibrary.push(book)
 	return myLibrary
 }
+
+function createBookElement(title, author, pages) {
+	const card = document.createElement('div')
+	const bookTitle = document.createElement('h3')
+	const bookAuthor = document.createElement('h3')
+	const numOfPages = document.createElement('p')
+	const removeBook = document.createElement('button')
+
+	card.classList.add('card')
+	bookTitle.classList.add('title')
+	bookAuthor.classList.add('author')
+	numOfPages.classList.add('pages')
+	removeBook.classList.add('remove-book')
+
+	bookTitle.innerText = title
+	bookAuthor.innerText = author
+	numOfPages.innerText = pages
+	removeBook.innerText = 'Remove'
+
+	card.appendChild(bookTitle)
+	card.appendChild(bookAuthor)
+	card.appendChild(numOfPages)
+	card.appendChild(removeBook)
+
+	booksGrid.appendChild(card)
+}
+
+function render() {
+	for (let i = 0; i < myLibrary.length; i++) {
+		createBookElement(
+			myLibrary[i].title,
+			myLibrary[i].author,
+			myLibrary[i].numOfPages
+		)
+	}
+}
+
+render()
